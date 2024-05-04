5e1b91d94eb58ea5b0a174b8988752e17f5e07ef
epdsant <epdsant@gmail.com>
2024-05-04 11:29:36 +0100

    feat: Created CategorySelect component and configured svg.d types to read svg image files

---
+++ src/components/CategorySelect/index.tsx
@@ -0,0 +1,17 @@
+import { ScrollView } from 'react-native-reanimated/lib/typescript/Animated';
+import { styles } from './styles';
+
+export function CategorySelect() {
+  return (
+    <ScrollView
+      style={styles.container}
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      contentContainerStyle={{ paddingRight: 40 }}
+    >
+        {
+            
+        }
+    </ScrollView>
+  );
+}
